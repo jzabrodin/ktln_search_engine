99fa73b43e439e6d9db00d9443a8d6f70284e0f0
Yevhen Zabrodin <jzabrodin@gmail.com>
2022-10-25 19:29:51 +0300

    Update Main.kt

--- Main.kt
+++ Main.kt
@@ -4,14 +4,144 @@ import java.io.File
 
 val map = mutableMapOf<String, MutableList<Int>>()
 
-class SearchEngine {
-    fun search(input: List<String>, word: String): Unit {
+enum class SearchStrategies(val mode: String) {
+    ALL("ALL"),
+    ANY("ANY"),
+    NONE("NONE");
+
+    companion object {
+        fun isCorrectValue(x: String): Boolean {
+            return SearchStrategies.values().find { it.mode == x } !== null
+        }
+
+        fun getEnumByString(x: String): SearchStrategies? {
+            return SearchStrategies.values().find { it.mode == x }
+        }
+
+        fun strategiesAsString(): String {
+            val data = mutableListOf<String>()
+            SearchStrategies.values().forEach { data.add(it.mode) }
+
+            return data.joinToString( ", " )
+        }
+    }
+
+    override fun toString(): String {
+        return mode;
+    }
+}
+
+class SearchStrategyAny{
+    fun search(
+        word: String,
+        input: List<String>
+    ): MutableList<String> {
         val result = mutableListOf<String>()
+        val searchWords = word.split(" ")
+
+        searchWords.forEach { searchWord ->
+            map[searchWord.lowercase()]?.forEach {
+                val element = input[it]
+                if(!result.contains(element)){
+                    result.add(element)
+                }
+            }
+        }
+        return result
+    }
+}
+
+class SearchStrategyNone{
+    fun search(
+        word: String,
+        input: List<String>
+    ): MutableList<String> {
+        val result = mutableListOf<String>()
+        val searchWords = word.split(" ")
+
+        val stringNumbersWithStopWords = mutableListOf<Int>()
+
+        searchWords.forEach { searchWord ->
+            map[searchWord.lowercase()]?.forEach {
+                if (!stringNumbersWithStopWords.contains(it)){
+                    stringNumbersWithStopWords.add(it)
+                }
+            }
+        }
+
+        for( i in 0..input.lastIndex){
+            if (stringNumbersWithStopWords.contains(i)){
+                continue
+            }
+
+            result.add(input[i])
+        }
+        return result
+    }
+}
+
+class SearchStrategyAll {
+    fun search(
+        searchWords: String,
+        input: List<String>
+    ): MutableList<String> {
+        val words = searchWords.split(" ")
+        val localCache = mutableMapOf<String, MutableList<String>>()
+
+        // fill local cache
+        words.forEach { word ->
+            map[word.lowercase()]?.forEach {
+                if (it < input.size) {
+                    if (localCache.containsKey(word)) {
+                        localCache[word]?.add(input[it])
+                    } else {
+                        val list = mutableListOf<String>()
+                        list.add(input[it])
+                        localCache[word] = list
+                    }
+                }
+            }
+        }
+
+        val result = mutableListOf<String>()
+        //check local cache
+        localCache.forEach { mapEntry ->
+            mapEntry.value.forEach { mapEntryValue ->
+
+                var isAllWordsPresentedInResult = true
+                for (word: String in words){
+                    val isContainWord = mapEntryValue.contains(word, true)
+                    if(!isContainWord){
+                        isAllWordsPresentedInResult = false
+                        break
+                    }
+                }
+                if (isAllWordsPresentedInResult && !result.contains(mapEntryValue)) {
+                    result.add(mapEntryValue)
+                }
+            }
+        }
+
+        return result
+    }
+}
+
+class SearchEngine {
+    fun search(input: List<String>, word: String, strategy: SearchStrategies): Unit {
+
         val wordProcessed = word.trim().lowercase()
 
-        map[wordProcessed]?.forEach {
-            if (it < input.size) {
-                result.add(input[it])
+        val result = when (strategy) {
+            SearchStrategies.ANY -> {
+                SearchStrategyAny().search(wordProcessed, input)
+            }
+
+            SearchStrategies.NONE -> {
+                SearchStrategyNone().search(wordProcessed, input)
+            }
+
+            SearchStrategies.ALL -> {
+                SearchStrategyAll().search(wordProcessed, input)
             }
         }
 
@@ -41,11 +171,11 @@ class DataCollector {
 }
 
 class QueriesProcessor {
-    fun process(inputData: List<String>): Unit {
+    fun process(inputData: List<String>, searchStrategy: SearchStrategies) {
         val engine = SearchEngine()
         println("Enter data to search people:")
-        val word = readln().trim()
-        engine.search(inputData, word)
+        val word = readln().trim().lowercase()
+        engine.search(inputData, word, searchStrategy)
     }
 }
 
@@ -94,7 +224,9 @@ class MenuProcessor(private val inputData: List<String>) {
 
         when (getOption()) {
             Options.FIND.option -> {
-                queriesProcessor.process(this.inputData)
+                val searchStrategy: SearchStrategies = this.getSearchStrategy()
+
+                queriesProcessor.process(this.inputData, searchStrategy)
                 this.process()
             }
 
@@ -107,6 +239,22 @@ class MenuProcessor(private val inputData: List<String>) {
                 return
             }
         }
+    }
+
+    private fun getSearchStrategy(): SearchStrategies {
+        println("Please select one of this strategy: " + SearchStrategies.strategiesAsString())
+
+        val strategy = readln()
+        var searchStrategy: SearchStrategies? = null
+
+        searchStrategy = SearchStrategies.getEnumByString(strategy)
+
+        if (searchStrategy == null) {
+            println("Incorrect value")
+            return this.getSearchStrategy()
+        }
+
+        return searchStrategy
     }
 
     private fun showMenu() {
@@ -148,6 +296,7 @@ class IndexCreator {
 }
 
 fun main(args: Array<String>) {
+
     var inputData: MutableList<String> = mutableListOf()
 
     if (args.isEmpty()) {
